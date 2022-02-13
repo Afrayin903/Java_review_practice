961968d0742b04a89a58fa88f565f382d55737d9
Kens <afrayin903@gmail.com>
2022-02-13 18:20:24 -0500

    Added new class

--- Generics/src/main/java/Team.java
+++ Generics/src/main/java/Team.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Team {
+
+    private String name;
+
+    private ArrayList<Player> members = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public boolean addPlayer(Player player) {
+
+      if(members.contains(player)){
+          System.out.println(player.getName() + "is already on this team ");
+          return false;
+      }else {
+          members.add(player);
+          System.out.println(player.getName() + "picked for team " + this.name);
+          return true;
+      }
+    }
+    public int runPlayers(){
+        return this.members.size();
+    }
 }
